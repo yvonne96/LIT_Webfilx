a038f2c5cffca3ea858dbe450b8b77fac0e94897
Brodigan <n0289147@lm.lmig.com>
2017-07-12 11:46:16 +0100

    V1.0 Moved logic from AccRestContr. to a call in the AccountService class

--- server/webflix/src/main/java/com/instil/webflix/security/service/AccountService.java
+++ server/webflix/src/main/java/com/instil/webflix/security/service/AccountService.java
@@ -1,7 +1,14 @@
 package com.instil.webflix.security.service;
 
+import org.springframework.http.ResponseEntity;
+
 import com.instil.webflix.security.model.Account;
+import com.instil.webflix.security.model.LoginResponse;
 
 public interface AccountService {
 	Account getCurrent();
+	
+	ResponseEntity<LoginResponse> login(String email, String pw);
+	
+	ResponseEntity<Object> register(Account newAccount);
 }
