7450e474922e73cd9472860fd67f7250b3fa3eae
Murphy <n0289138@lm.lmig.com>
2017-07-20 15:34:35 +0100

    completed html of manage vouchers

--- client/webflix/src/app/component/admin-tools/manage-vouchers/manage-vouchers.component.ts
+++ client/webflix/src/app/component/admin-tools/manage-vouchers/manage-vouchers.component.ts
@@ -10,5 +10,46 @@ import {Router} from '@angular/router';
   templateUrl: 'manage-vouchers.component.html'
 })
 export class ManageVouchersComponent {
+  private toggledBuyXGetYFree: boolean = false;
+  private toggledPercentOff: boolean = false;
+  private toggledSpendXGetYOff: boolean = false;
+  private menuType: String;
 
-}
+  constructor() {
+
+  }
+
+    setValue(menuSelection : String){
+    this.menuType = menuSelection;
+    this.chooseDiscountMenu();
+  }
+
+  chooseDiscountMenu() {
+
+    switch (this.menuType) {
+      case 'buyXGetY':
+        this.toggledPercentOff = false;
+        this.toggledBuyXGetYFree = true;
+        this.toggledSpendXGetYOff = false;
+        break;
+
+      case 'percentage':
+        this.toggledPercentOff = true;
+        this.toggledBuyXGetYFree = false;
+        this.toggledSpendXGetYOff = false;
+        break;
+
+      case 'spendXGetY':
+        this.toggledPercentOff = false;
+        this.toggledBuyXGetYFree = false;
+        this.toggledSpendXGetYOff = true;
+        break;
+
+      default:
+        this.toggledPercentOff = false;
+        this.toggledBuyXGetYFree = false;
+        this.toggledSpendXGetYOff = false; }
+  }}
+
+
+
