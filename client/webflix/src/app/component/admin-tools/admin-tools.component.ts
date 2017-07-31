154484d763340472ad3899d19c9c360fcc284ace
Murphy <n0289138@lm.lmig.com>
2017-07-31 12:06:58 +0100

    added non-admin restrictions to admin sites

--- client/webflix/src/app/component/admin-tools/admin-tools.component.ts
+++ client/webflix/src/app/component/admin-tools/admin-tools.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {AuthenticationService} from '../../service/authentication/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
   moduleId: module.id,
@@ -6,5 +8,10 @@ import {Component} from '@angular/core';
   templateUrl: 'admin-tools.component.html'
 })
 export class AdminToolsComponent {
-
+  private isAdmin: boolean;
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService) {
+    authenticationService.isAdmin
+      .subscribe(x => this.isAdmin = x);
+  }
 }
