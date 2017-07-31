d834f89df7611640a9250d32421d174b1a171c27
Murphy <n0289138@lm.lmig.com>
2017-07-31 15:06:00 +0100

    added baymax to errpr message

--- client/webflix/src/app/component/error/error.component.ts
+++ client/webflix/src/app/component/error/error.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'error',
-  template: '<div><h1>Whooops!!!!</h1></div>'
+  template: '<div><h1>Whooops!!!!</h1><img src="app/component/error/jumpOutWindow.jpg" height = "400" width="400"></div>'
 })
 export class ErrorComponent {
 }
