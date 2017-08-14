93f7c202c74d5446846c1b0a38eba2ca8c724ffd
Brodigan <n0289147@lm.lmig.com>
2017-08-14 15:49:35 +0100

    Added wishlist functionality to detailed movie view

--- client/webflix/src/app/component/detailed-movie-view/detailed-movie-view.component.ts
+++ client/webflix/src/app/component/detailed-movie-view/detailed-movie-view.component.ts
@@ -9,6 +9,8 @@ import {BasketSummary} from '../../model/basket-summary';
 import {ReviewService} from '../../service/review/review.service';
 import {ApiClient} from '../../service/api-client/api-client.service';
 import {Review} from '../../model/review';
+import {WishlistService} from '../../service/wishlist/wishlist.service';
+import {WishlistSummary} from '../../model/wishlist-summary';
 
 @Component({
   moduleId: module.id,
@@ -22,8 +24,10 @@ export class DetailedMovieViewComponent {
   public theMovieID: number;
   public owned: boolean;
   public inBasket: boolean;
+  public inWishlist: boolean;
   public summary: BasketSummary;
   public myMovies: Movie[];
+  public wishlist: WishlistSummary;
   public score: number;
   public comments: string;
   public currentUserID: number;
@@ -37,13 +41,15 @@ export class DetailedMovieViewComponent {
               private basketService: BasketService,
               private location: Location,
               private reviewService: ReviewService,
-              private apiClient: ApiClient) {
+              private apiClient: ApiClient,
+              private wishlistService: WishlistService) {
     this.summary = BasketSummary.empty();
     this.pullIdFromParams();
     this.retrieveMovieData(this.movieService.fetchById(Number(this.theMovieID)));
     this.retrieveAvgReviewScore(this.reviewService.getAvgScoreByID(Number(this.theMovieID)));
     this.readBasketForUser();
     this.readMyMoviesForUser();
+    this.readWishlistForUser();
     this.retrieveCurrentAccountID(this.apiClient.getCurrentAccountID());
     this.retrieveReviewsForMovie(this.reviewService.getReviewsByMovieID(this.theMovieID));
   }
@@ -74,6 +80,12 @@ export class DetailedMovieViewComponent {
     }
   }
 
+  addMovieToWishlist() {
+    this.wishlistService.addToWishlist(this.theMovie)
+      .subscribe();
+    this.inWishlist = true;
+  }
+
   readBasketForUser() {
     this.basketService.getBasketSummary()
       .subscribe(summary => {
@@ -94,6 +106,16 @@ export class DetailedMovieViewComponent {
       }, error => ('Error pulling user movies'));
   }
 
+  readWishlistForUser() {
+    this.wishlistService.getWishlistSummary()
+      .subscribe(wishlist => {
+       this.wishlist = wishlist;
+       if (this.checkForWishlistDuplicates()) {
+         this.inWishlist = true;
+       }
+    }, error => ('Error pulling user wishlist'));
+  }
+
   checkForMovieDuplicates(): boolean {
     let checkMovies: Movie[] = this.myMovies;
     for (let m = 0; m < checkMovies.length; m++) {
@@ -108,6 +130,16 @@ export class DetailedMovieViewComponent {
     let basketMovies: Movie[] = this.summary.movies;
     for (let n = 0; n < basketMovies.length; n++) {
       if (Number(this.theMovieID) === basketMovies[n].id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkForWishlistDuplicates() {
+    let wishlistMovies: Movie[] = this.wishlist.movies;
+    for (let n = 0; n < wishlistMovies.length; n++) {
+      if (this.theMovie.id === wishlistMovies[n].id) {
         return true;
       }
     }
